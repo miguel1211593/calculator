fabb9611e5be52cb2d4d2b2a8f94e6f308f847c0
miguel1211593 <106148619+miguel1211593@users.noreply.github.com>
2024-01-09 15:34:18 +0000

    ff

--- calculadora-react/src/components/SpecialButton.js
+++ calculadora-react/src/components/SpecialButton.js
@@ -2,11 +2,13 @@
 import React from 'react';
 
 const SpecialButton = ({ value, image, onClick }) => {
+
   return (
     <button className="specialCharacters" onClick={onClick}>
       {image ? <img src={image} alt={value} className="button-image"/> : value}
     </button>
   );
+  
 };
 
 export default SpecialButton;
